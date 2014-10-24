5d27a9130bd70ea1bb0f3dcefd0c658ada584d89
寺内 信夫 <tenobu@gmail.com>
2014-10-24 11:29:29 +0900

    Initial Commit

---
+++ PhotoWake/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PhotoWake
+//
+//  Created by 寺内 信夫 on 2014/10/24.
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
