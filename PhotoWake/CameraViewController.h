2a665406e36839826d85c87056d43abfe540dd38
寺内 信夫 <tenobu@gmail.com>
2014-10-24 14:51:20 +0900

    カメラ

---
+++ PhotoWake/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  PhotoWake
+//
+//  Created by 寺内 信夫 on 2014/10/24.
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
---
+++ PhotoWake/CameraViewController.m
@@ -0,0 +1,37 @@
+//
+//  CameraViewController.m
+//  PhotoWake
+//
+//  Created by 寺内 信夫 on 2014/10/24.
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import "CameraViewController.h"
+
+@interface CameraViewController ()
+
+@end
+
+@implementation CameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
